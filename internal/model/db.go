97ff9ac1ff66c49dd99bcc656f1c7cdac18e3cbb
Xiwon <3550216663@qq.com>
2024-03-16 02:50:10 +0000

    style: add user.go style: remove 2 deprecated functions

--- internal/model/db.go
+++ internal/model/db.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 
 	"bbyd/internal/shared/config"
-	"bbyd/internal/controllers/auth"
+	// "bbyd/internal/controllers/auth"
 	// "bbyd/pkg/utils/logs"
 
 	// "go.uber.org/zap"
@@ -26,14 +26,6 @@ var (
 
 var db *gorm.DB           // postgresql
 var redisConn redis.Conn // redis
-
-type UserModel struct {
-	gorm.Model
-	Username string
-	Secret   string
-	Email    string
-	Auth     string
-}
 
 func AutoMigrate(d config.Database) error {
 	err := db.AutoMigrate(
@@ -66,137 +58,6 @@ func AutoMigrate(d config.Database) error {
 	for _, v := range usrdatas {
 		fmt.Printf("{ Username:%s, Email:%s, Auth:%s }\n", 
 			v.Username, v.Email, v.Auth)
-	}
-
-	return nil
-}
-
-// usr, err := model.GetUsrByName(name)
-func GetUsrByName(name string) (UserModel, error) {
-	var user UserModel
-	result := db.First(&user, "username = ?", name)
-	if result.Error != nil {
-		if result.Error == gorm.ErrRecordNotFound {
-			return UserModel{}, DBNotFoundError
-		}
-		return UserModel{}, DBInternalError
-	}
-
-	return user, nil
-}
-
-// db_sec, err := GetSecretByName(req.Name)
-func GetSecretByName(name string) (string, error) {
-	var user UserModel
-	result := db.First(&user, "username = ?", name)
-	if result.Error != nil {
-		if result.Error == gorm.ErrRecordNotFound {
-			return "", DBNotFoundError
-		}
-		return "", DBInternalError
-	}
-	
-	return user.Secret, nil
-}
-
-// err = TryRegister(req.Name, req.Passwd, req.Email)
-func TryRegister(name string, passwd string, email string) error {
-	var user UserModel
-	result := db.First(&user, "username = ?", name)
-	if result.Error != nil && result.Error != gorm.ErrRecordNotFound {
-		return DBInternalError
-	}
-	if result.RowsAffected > 0 {
-		return DBExistError
-	}
-	user = UserModel{
-		Username: name,
-		Secret:   auth.GenerateSecret(passwd, auth.GenerateSalt()),
-		Email:    email,
-		Auth:     config.Configs.Constants.DefaultAuthname,
-	}
-	err := db.Create(&user).Error
-	if err != nil {
-		return DBInternalError
-	}
-	return nil
-}
-
-// msg, err := TryChangeInfo(req.Name, req.Passwd, req.Email)
-func TryChangeInfo(name string, passwd string, email string, authoriz string) (string, error) {
-	var user UserModel
-	result := db.First(&user, "username = ?", name)
-	if result.Error != nil && result.Error != gorm.ErrRecordNotFound {
-		return "", DBInternalError
-	}
-	if result.Error == gorm.ErrRecordNotFound {
-		return "user " + name + " not found", DBNotFoundError
-	}
-
-	var msg string
-	if passwd != "" {
-		user.Secret = auth.GenerateSecret(passwd, auth.GenerateSalt())
-		msg += " passwd changed"
-	}
-	if email != "" {
-		user.Email = email
-		msg += " email changed to " + email
-	}
-	if authoriz != "" {
-		user.Auth = authoriz
-		msg += " auth changed to " + authoriz
-	}
-	if msg == "" {
-		msg = "nothing changed"
-	} else {
-		err := db.Save(&user).Error
-		if err != nil {
-			return "", DBInternalError
-		}
-	}
-
-	return msg, nil
-}
-
-// msg, err := TryDelete(req.Name)
-func TryDelete(name string) (string, error) {
-	var user UserModel
-	result := db.First(&user, "username = ?", name)
-	if result.Error != nil && result.Error != gorm.ErrRecordNotFound {
-		return "", DBInternalError
-	}
-	if result.Error == gorm.ErrRecordNotFound {
-		return "user " + name + " not found", DBNotFoundError
-	}
-	err := db.Delete(&user).Error
-	if err != nil {
-		return "", DBInternalError
-	}
-	return "delete user " + name, nil
-}
-
-func GetEmailLastSendTime(email string) (interface{}, error) {
-	las, err := redisConn.Do("get", "emailLastSendTime:" + email)
-	if err != nil {
-		return "", RedisInternalError
-	}
-	return las, nil
-}
-
-func UpdateCodeSendRecord(email string, code string, name string) error {
-	_, err := redisConn.Do("set", "emailLastSendTime:" + email, fmt.Sprintf("%d", time.Now().Unix()))
-	if err != nil {
-		return RedisInternalError
-	}
-
-	_, err = redisConn.Do("set", "codeUser:" + code, name)
-	if err != nil {
-		return RedisInternalError
-	}
-
-	_, err = redisConn.Do("set", "codeSendTime:" + code, fmt.Sprintf("%d", time.Now().Unix()))
-	if err != nil {
-		return RedisInternalError
 	}
 
 	return nil
